11c92ea1026c70677826b84006119e4249eff61f
Jacob Dimuzio <jacob.dimuzio@spothero.com>
2025-11-03 11:03:42 -0600

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import act from "react";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
